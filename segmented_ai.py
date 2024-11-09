c5129e372f59ac4bb684492b495ae83bd0d7e092
Saesenthessis66 <lucjangruby@gmail.com>
2024-11-09 12:28:44 +0100

    Delete second scaler

--- segmented_ai.py
+++ segmented_ai.py
@@ -27,41 +27,29 @@ if __name__ == '__main__':
         # first_segment = close_segments[i][0]
         # second_segment = close_segments[i][1]
 
-        first_segment = 20.0
-        second_segment = 48.0
+        first_segment = 56.0
+        second_segment = 20.0
 
         df_first = segment_dict[str(first_segment)]
         df_second = segment_dict[str(second_segment)]
 
         df = pd.concat([df_first, df_second], ignore_index=True)
 
-        # plt.plot(df['X-coordinate'],df['Y-coordinate'])
-        # plt.title('segments_' + str(first_segment) + '_' + str(second_segment))
-        # plt.show()
-
-        # Normalize the data for features (first scaler)
-        scaler_features = MinMaxScaler()
+        # Normalize the data 
+        scaler = MinMaxScaler()
         df_scaled = df.copy()
-        df_scaled[['X-coordinate', 'Y-coordinate', 'Heading']] = scaler_features.fit_transform(
-            df[['X-coordinate', 'Y-coordinate','Heading']]
-        )
-
-        # Normalize the target data (second scaler)
-        scaler_target = MinMaxScaler()
-        df_target_scaled = scaler_target.fit_transform(df[['X-coordinate', 'Y-coordinate','Heading']])
+        df_scaled = scaler.fit_transform(df)
 
         # Create sequences (using df_scaled for features and df_target_scaled for targets)
-        def create_sequences(df_features, df_target, n_steps):
+        def create_sequences(data, n_steps):
             X, y = [], []
-            for i in range(len(df_features) - n_steps):
-                X.append(df_features.iloc[i:i+n_steps].values)
-                y.append(df_target[i+n_steps])
+            for i in range(len(data) - n_steps):
+                X.append(data[i:i+n_steps])
+                y.append(data[i+n_steps])
             return np.array(X), np.array(y)
 
         n_steps = 20
-        X, y = create_sequences(df_scaled[[ 'X-coordinate', 'Y-coordinate','Heading' ]],
-                                df_target_scaled, n_steps)
-
+        X, y = create_sequences(df_scaled, n_steps)
 
         model = Sequential([
             LSTM(5, input_shape=(n_steps, 3)),
@@ -69,10 +57,10 @@ if __name__ == '__main__':
             Dense(32, activation='relu'),
             Dense(3)
         ])
-        optimizer = tf.keras.optimizers.Adam(learning_rate=0.001, clipvalue=1.2)
-        model.compile(optimizer=optimizer, loss='mse')
+        optimizer = tf.keras.optimizers.Adam(learning_rate=0.001, clipvalue=1.0)
+        model.compile(optimizer=optimizer, loss='mae')
 
         # Train the model
-        model.fit(X, y, epochs=600, batch_size=32, verbose=1)
+        model.fit(X, y, epochs=1000, batch_size=32, verbose=1)
 
         model.save('kerases/segments_' + str(first_segment) + '_' + str(second_segment) + '.keras')
